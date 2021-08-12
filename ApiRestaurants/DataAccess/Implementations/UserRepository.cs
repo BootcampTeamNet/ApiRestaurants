4ca436620798886f7c0110a804c5fc33dd16626c
HENRY GARCIA OSPINA <henrygarcia0309@gmail.com>
2021-08-12 10:17:13 -0500

    Creation DTO option update dish

--- ApiRestaurants/DataAccess/Implementations/UserRepository.cs
+++ ApiRestaurants/DataAccess/Implementations/UserRepository.cs
@@ -1,10 +1,6 @@
 ﻿using DataAccess.Interfaces;
 using Entities;
 using Microsoft.EntityFrameworkCore;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
 using System.Threading.Tasks;
 
 namespace DataAccess.Implementations
---
+++ ApiRestaurants/DataAccess/Interfaces/IDishRepository.cs
@@ -0,0 +1,11 @@
+﻿using Entities;
+using System.Threading.Tasks;
+
+namespace DataAccess.Interfaces
+{
+    public interface IDishRepository
+    {
+        Task<bool> ExistDish(int id);
+        Task<Dish> GetDish(string id);
+    }
+}
