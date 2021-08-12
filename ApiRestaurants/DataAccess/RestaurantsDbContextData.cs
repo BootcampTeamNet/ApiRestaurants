c1d20f1e063e63c7e2d2642c9414e1034b9b1fbc
santiago <jssalgadog@gmail.com>
2021-08-12 15:32:32 -0500

    feat(DataAccess/RestaurantsDbContextData) Load the json Data to the table DishCategory

---
+++ ApiRestaurants/DataAccess/RestaurantsDbContextData.cs
@@ -0,0 +1,43 @@
+﻿using Entities;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text.Json;
+using System.Threading.Tasks;
+
+namespace DataAccess
+{
+    public class RestaurantsDbContextData
+    {
+        public static async Task LoadDataAsync(RestaurantsDbContext context,
+                                                ILoggerFactory loggerFactory)
+        {
+            try
+            {
+
+                if (!context.DishCategories.Any())
+                {
+                    var dishCategoriesData = File.ReadAllText("../DataAccess/LoadData/DishCategory.json");
+                    var dishCategories = JsonSerializer.Deserialize<List<DishCategory>>(dishCategoriesData);
+
+                    foreach (var category in dishCategories)
+                    {
+                        context.DishCategories.Add(category);
+                    }
+
+                    await context.SaveChangesAsync();
+                }               
+
+            }
+            catch (Exception e)
+            {
+                var logger = loggerFactory.CreateLogger<RestaurantsDbContext>();
+                logger.LogError(e.Message);
+            }
+        }
+
+
+    }
+}
