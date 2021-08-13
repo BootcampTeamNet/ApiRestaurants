818a18f8390e139683f2f337d3cdf708bc3f3342
veroely <vero_ely_vc@otmail.com>
2021-08-13 16:52:29 -0500

    add json restaurant categories

--- ApiRestaurants/DataAccess/RestaurantsDbContextData.cs
+++ ApiRestaurants/DataAccess/RestaurantsDbContextData.cs
@@ -22,14 +22,25 @@ namespace DataAccess
                     var dishCategoriesData = File.ReadAllText("../DataAccess/LoadData/DishCategory.json");
                     var dishCategories = JsonSerializer.Deserialize<List<DishCategory>>(dishCategoriesData);
 
-                    foreach (var category in dishCategories)
+                    foreach (var dishCategory in dishCategories)
                     {
-                        context.DishCategories.Add(category);
+                        context.DishCategories.Add(dishCategory);
                     }
 
                     await context.SaveChangesAsync();
-                }               
+                }
+                if (!context.RestaurantCategories.Any())
+                {
+                    var restaurantCategoriesData = File.ReadAllText("../DataAccess/LoadData/RestaurantCategories.json");
+                    var restaurantCategories = JsonSerializer.Deserialize<List<RestaurantCategory>>(restaurantCategoriesData);
 
+                    foreach (var restaurantCategory in restaurantCategories)
+                    {
+                        context.RestaurantCategories.Add(restaurantCategory);
+                    }
+
+                    await context.SaveChangesAsync();
+                }
             }
             catch (Exception e)
             {
