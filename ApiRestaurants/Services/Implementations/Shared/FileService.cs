f34d3a807af91d6c15c8713b87c2510fe8992b18
veroely <vero_ely_vc@otmail.com>
2021-08-13 12:36:56 -0500

    add fileservice for save files local

---
+++ ApiRestaurants/Services/Implementations/Shared/FileService.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Http;
+using Services.Interfaces;
+using System.IO;
+using System.Threading.Tasks;
+
+namespace Services.Implementations.Shared
+{
+    public class FileService : IFileService
+    {
+        public async Task SaveFile(IFormFile file, string subDirectory)
+        {
+            subDirectory = subDirectory ?? string.Empty;
+            var target = Path.Combine("C:\\Desktop\\Restaurants\\", subDirectory);
+
+            Directory.CreateDirectory(target);
+
+            if (file.Length <= 0) return;
+            var filePath = Path.Combine(target, file.FileName);
+            using (var stream = new FileStream(filePath, FileMode.Create))
+            {
+                await file.CopyToAsync(stream);
+            }
+        }
+
+        public void DeleteFile(string path)
+        {
+            Directory.Delete(path, true);
+        }
+    }
+}
