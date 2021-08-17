05e2922443ebc9d35b9e3071cf03422e66fcb270
HENRY GARCIA OSPINA <henrygarcia0309@gmail.com>
2021-08-16 23:24:33 -0500

    correction add fields dto  DishesByRestaurantResponseDto

--- ApiRestaurants/DTOs/Dish/DishesByRestaurantResponseDto.cs
+++ ApiRestaurants/DTOs/Dish/DishesByRestaurantResponseDto.cs
@@ -4,6 +4,17 @@
     {
         //Info del plato
         public int Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public decimal Price { get; set; }
+        public string PathImage { get; set; }
+        public int CaloriesMinimun { get; set; }
+        public int CaloriesMaximun { get; set; }
+        public int Proteins { get; set; }
+        public int Fats { get; set; }
+        public int Sugars { get; set; }
+        public bool IsActive { get; set; }
+        public int DishCategoryId { get; set; }
         public int RestaurantId { get; set; }
     }
 }
