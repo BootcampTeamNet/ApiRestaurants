ae7ad0364ca4d01035f5b8cac2d1109fa77bc5e4
santiago <jssalgadog@gmail.com>
2021-08-19 11:37:19 -0500

    fix(BranchOfficeService) Add somo configurations to modify the logical add in a branch

--- ApiRestaurants/Services/Implementations/Restaurants/BranchOfficeService.cs
+++ ApiRestaurants/Services/Implementations/Restaurants/BranchOfficeService.cs
@@ -11,16 +11,23 @@ namespace Services.Implementations.Restaurants
 {
     public class BranchOfficeService: IBranchOfficeService
     {
+        //private readonly IRestaurantRepository _restaurantRepository;
+        //private readonly IUserService _userService;
+        //private readonly IPasswordService _passwordService;
         private readonly IGenericRepository<Restaurant> _genericRepository;
-        private readonly IDishRepository _iDishRepository;
         private readonly IMapper _mapper;
 
         public BranchOfficeService(
+            //IRestaurantRepository restaurantRepository,
+            //IUserService userService,
+            //IPasswordService passwordService,
             IGenericRepository<Restaurant> genericRepository,
-            IDishRepository _iDishRepository,
             IMapper mapper
             )
         {
+            //_restaurantRepository = restaurantRepository;
+            //_userService = userService;
+            //_passwordService = passwordService;
             _genericRepository = genericRepository;
             _mapper = mapper;
         }
@@ -35,6 +42,35 @@ namespace Services.Implementations.Restaurants
             {
                 throw new Exception("Los campos no pueden ser nulos");
             }
+
+            /*
+            bool exist = await _userService.ExistsUser(restaurantRequestDto.User.Email);
+            if (exist)
+            {
+                throw new Exception($"Ya existe un usuario registrado con el email {restaurantRequestDto.User.Email}");
+            }
+
+            UserRestaurant userRestaurant = new UserRestaurant();
+            userRestaurant.Restaurant = new Restaurant
+            {
+                Name = restaurantRequestDto.Name,
+                Address = restaurantRequestDto.Address,
+                LocationLatitude = restaurantRequestDto.LocationLatitude,
+                LocationLongitude = restaurantRequestDto.LocationLongitude
+            };
+            //arma el objeto del usuario para el resturante
+            _passwordService.CreatePasswordHash(restaurantRequestDto.User.Password, out byte[] passwordHash, out byte[] passwordSalt);
+            userRestaurant.User = new User
+            {
+                FirstName = restaurantRequestDto.User.FirstName,
+                LastName = restaurantRequestDto.User.LastName,
+                Email = restaurantRequestDto.User.Email,
+                PasswordHash = passwordHash,
+                PasswordSalt = passwordSalt
+            };
+
+            return await _restaurantRepository.Add(userRestaurant);
+            */        
 
             var data = _mapper.Map<Restaurant>(branchOfficeRequestDto);
             await _genericRepository.Add(data);
