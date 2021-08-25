7e67ad867ece6d6976cc311ae9a01264e1807281
veroely <vero_ely_vc@otmail.com>
2021-08-25 13:27:31 -0500

    correction constant

--- ApiRestaurants/DTOs/Constants/Constant.cs
+++ ApiRestaurants/DTOs/Constants/Constant.cs
@@ -4,8 +4,8 @@
     {
         public static class BookingStatus
         {
-            public const string PENDIENTE = "Confirmada";
-            public const string CONFIRMADA = "Confirmada";
+            public const string PENDIENTE = "Pendiente";
+            public const string CONFIRMADA = "Confirmado";
             public const string CANCELADA_RESTAURANT = "Cancelado por Restaurante";
             public const string CANCELADA_COMENSAL = "Cancelado por Comensal";
         }
