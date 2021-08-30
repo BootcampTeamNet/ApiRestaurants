bda682a0515dba8a6cf7d9288c09044b15294d9d
veroely <vero_ely_vc@otmail.com>
2021-08-30 13:50:48 -0500

    modify name branch, and add service get branches

---
+++ ApiRestaurants/DataAccess/Implementations/BranchRepository.cs
@@ -0,0 +1,22 @@
+﻿using DataAccess.Interfaces;
+using Entities;
+using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DataAccess.Implementations
+{
+    public class BranchRepository: IBranchRepository
+    {
+        private readonly RestaurantsDbContext _context;
+        public BranchRepository(RestaurantsDbContext context)
+        {
+            _context = context;
+        }
+        public async Task<List<Restaurant>> GetByRestaurantId(int id)
+        {
+            return await _context.Restaurants.Where(w => w.MainBranchId == id).ToListAsync();
+        }
+    }
+}
