c004e738b9c988941ce880257ea10f80fe9bbbd8
veroely <vero_ely_vc@otmail.com>
2021-09-01 09:19:45 -0500

    correct miskate to merge

--- ApiRestaurants/DataAccess/Implementations/FavouriteRepository.cs
+++ ApiRestaurants/DataAccess/Implementations/FavouriteRepository.cs
@@ -1,6 +1,7 @@
 ﻿using DataAccess.Interfaces;
 using Entities;
 using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
--- ApiRestaurants/DataAccess/Interfaces/IFavouriteRepository.cs
+++ ApiRestaurants/DataAccess/Interfaces/IFavouriteRepository.cs
@@ -1,4 +1,5 @@
 ﻿using Entities;
+using System.Collections.Generic;
 using System.Threading.Tasks;
 
 namespace DataAccess.Interfaces
